36c993ddfcb42c692819cc249e74a28b7d9df5a6
alexmenov <alexmenov@pop-os.localdomain>
2023-11-09 19:35:05 +0700

    added test

--- src/test/java/ru/airlines/AirlinesApplicationTests.java
+++ src/test/java/ru/airlines/AirlinesApplicationTests.java
@@ -3,11 +3,14 @@ package ru.airlines;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class AirlinesApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
